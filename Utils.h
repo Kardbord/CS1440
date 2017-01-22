fda5b1f1e4350f52ee77fc2b727a66908a654961
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-01-21 17:45:47 -0700

    Initial commit after cloning and re-organizing Instructor's code

---
+++ Utils.h
@@ -0,0 +1,21 @@
+//
+// Created by Stephen Clyde on 1/16/17.
+//
+
+#ifndef ANALYSTCOMPARER_UTILS_H
+#define ANALYSTCOMPARER_UTILS_H
+
+#include <string>
+#include <fstream>
+
+bool split(const std::string& s, char delimiter, std::string elements[], int expectedNumberOfElements);
+
+std::string getTrimmedLine(std::ifstream& inputStream);
+std::string trim(const std::string& str);
+std::string ltrim(const std::string& str);
+std::string rtrim(const std::string& str);
+bool IsNotWhiteSpace (char ch);
+
+std::string centerString(const std::string& str, int width);
+
+#endif //ANALYSTCOMPARER_UTILS_H
