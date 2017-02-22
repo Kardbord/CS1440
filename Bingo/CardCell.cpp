2d709b6afccaf4cae1f45794f3ebb69966fbc44d
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-02-21 19:43:44 -0700

    Created CardCell class

---
+++ Bingo/CardCell.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tanner on 2/21/2017.
+//
+
+#include "CardCell.h"
---
+++ Bingo/CardCell.h
@@ -0,0 +1,14 @@
+//
+// Created by Tanner on 2/21/2017.
+//
+
+#ifndef BINGO_CARDCELL_H
+#define BINGO_CARDCELL_H
+
+
+class CardCell {
+
+};
+
+
+#endif //BINGO_CARDCELL_H
