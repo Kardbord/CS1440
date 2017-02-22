a79be7c86b2cee9f217ac9b7b7d598e5eebf2d15
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-02-21 21:16:53 -0700

    Fixed bug in Card.cpp Card() where m_possNums was incorrect

--- Bingo/Card.cpp
+++ Bingo/Card.cpp
@@ -5,16 +5,16 @@
 #include <algorithm>
 #include "Card.h"
 
-Card::Card(int const & cardSize, int const & numberMax) : m_cardSize(cardSize), m_numberMax(numberMax) {
+Card::Card(int const &cardSize, int const &numberMax) : m_cardSize(cardSize), m_numberMax(numberMax) {
 
-    for (int i = 0; i < (2 * m_cardSize * m_cardSize); ++i){
+    for (int i = 0; i < m_numberMax; ++i) {
         m_possNums.push_back(i);
     }
 
     std::random_shuffle(m_possNums.begin(), m_possNums.end());
 
     int totCells = m_cardSize * m_cardSize;
-    for (int i = 0; i < totCells; ++i){
+    for (int i = 0; i < totCells; ++i) {
         m_cells.push_back(CardCell(m_possNums[i]));
     }
 }
