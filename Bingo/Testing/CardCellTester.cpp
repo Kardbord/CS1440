2cba1508836b41ecec81255432e23b5004742041
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-02-21 20:54:04 -0700

    Reformatted code for prettiness

--- Bingo/Testing/CardCellTester.cpp
+++ Bingo/Testing/CardCellTester.cpp
@@ -6,24 +6,24 @@
 #include "CardCellTester.h"
 #include "../CardCell.h"
 
-void CardCellTester::testGetVal(){
+void CardCellTester::testGetVal() {
     std::cout << std::endl << "Test Suite: CardCellTester::testGetVal" << std::endl;
 
     CardCell cell1(50);
 
-    if (cell1.getVal() != 50){
+    if (cell1.getVal() != 50) {
         std::cout << "Error in cell1.getVal: should be 50" << std::endl;
     }
 
     CardCell cell2(0);
 
-    if (cell2.getVal() != 0){
+    if (cell2.getVal() != 0) {
         std::cout << "Error in cell2.getVal: should be 50" << std::endl;
     }
 
     CardCell cell3(-50);
 
-    if (cell3.getVal() != -50){
+    if (cell3.getVal() != -50) {
         std::cout << "Error in cell3.getVal: should be -50" << std::endl;
     }
 }
