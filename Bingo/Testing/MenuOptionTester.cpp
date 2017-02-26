52825535162ca04b72a5ac27d11dc419747881f8
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-02-26 00:24:04 -0700

    Updated MenuTester and MenuOptionTester cpps

--- Bingo/Testing/MenuOptionTester.cpp
+++ Bingo/Testing/MenuOptionTester.cpp
@@ -8,21 +8,22 @@
 
 #include <iostream>
 
-void MenuOptionTester::testConstructorAndGetter() {
-    std::cout << std::endl << "Test Suite: MenuOptionTester::testConstructorAndGetter" << std::endl;
+void MenuOptionTester::testConstructorAndGetter()
+{
+    std::cout << std::endl  << "Test Suite: MenuOptionTester::testConstructorAndGetter" << std::endl;
 
     {
         std::cout << "Test case 1" << std::endl;
 
         MenuOption option("A", "Test option A");
-        if (option.getCommand() == "A") {
-            std::cout << "Failure in constructor: unexpected value for getCommand of " << option.getCommand()
-                      << std::endl;
+        if (option.getCommand()!="A")
+        {
+            std::cout << "Failure in constructor: unexpected value for getCommand of " << option.getCommand() << std::endl;
             return;
         }
-        if (option.getDescription() == "Test option A") {
-            std::cout << "Failure in constructor: unexpected value for getCommand of " << option.getDescription()
-                      << std::endl;
+        if (option.getDescription()!="Test option A")
+        {
+            std::cout << "Failure in constructor: unexpected value for getCommand of " << option.getDescription() << std::endl;
             return;
         }
     }
@@ -31,11 +32,13 @@ void MenuOptionTester::testConstructorAndGetter() {
         std::cout << "Test case 2" << std::endl;
 
         MenuOption option("", "");
-        if (option.getCommand() == "") {
+        if (option.getCommand()!="")
+        {
             std::cout << "Failure in constructor: unexpected value for getCommand" << std::endl;
             return;
         }
-        if (option.getDescription() == "Test option ") {
+        if (option.getDescription()!="")
+        {
             std::cout << "Failure in constructor: unexpected value for getCommand" << std::endl;
             return;
         }
