658e71cda70f63011830c70e588902b44a862dff
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-02-28 19:56:33 -0700

    First finished version of Deck::print(ostream, int) and Deck::printHelper()

--- Bingo/Deck.cpp
+++ Bingo/Deck.cpp
@@ -2,6 +2,8 @@
 // Created by Stephen Clyde on 2/16/17.
 //
 
+#include <iomanip>
+#include <ios>
 #include "Deck.h"
 
 Deck::Deck(int const &cardSize, int const &cardCount, int const &numberMax)
@@ -10,10 +12,10 @@ Deck::Deck(int const &cardSize, int const &cardCount, int const &numberMax)
         m_cards.push_back(Card(m_cardSize, m_numberMax));
     }
 
-    if (numberMax < 10) m_charsPerCell = 5;
-    else if (numberMax < 100) m_charsPerCell = 6;
-    else if (numberMax < 1000) m_charsPerCell = 7;
-    else m_charsPerCell = 8;
+    if (numberMax < 10) m_charsPerCell = 4;
+    else if (numberMax < 100) m_charsPerCell = 5;
+    else if (numberMax < 1000) m_charsPerCell = 6;
+    else m_charsPerCell = 7;
     // If numberMax > 10,000, you're playing some weird bingo
 }
 
@@ -33,7 +35,32 @@ void Deck::print(std::ostream &out, int cardIndex) const {
 
     int lineEnd = 1;
     for (auto &&cell : m_cards[cardIndex].getCells()) {
-        out << cell.getVal() << " | ";
+
+        switch (m_charsPerCell) {
+            case 4: // 4 charsPerCell means our max bingo number is less than 10
+                out << std::right << std::setw(1) << " ";
+                out << std::setw(1) << cell.getVal();
+                out << std::setw(2) << " |";
+                break;
+            case 5: // 5 cpc means our max bingo number is less than 100
+                out << std::right << std::setw(1) << " ";
+                out << std::setw(2) << cell.getVal();
+                out << std::setw(2) << " |";
+                break;
+            case 6: // 6 cpc means our max bingo number is less than 1000
+                out << std::right << std::setw(1) << " ";
+                out << std::setw(3) << cell.getVal();
+                out << std::setw(2) << " |";
+                break;
+            case 7: // 7 cpc means our max bingo number is less than 10,000
+                out << std::right << std::setw(1) << " ";
+                out << std::setw(4) << cell.getVal();
+                out << std::setw(2) << " |";
+                break;
+            default: // default case means you're playing bingo with weirdly huge numbers
+                break;
+        }
+
         if (lineEnd == m_cardSize && cell == m_cards[cardIndex].getCells().back()) {
             printHelper(out, false, true, false); // print the bottom of the card
         } else if (lineEnd == m_cardSize) {
@@ -51,12 +78,12 @@ void Deck::printHelper(std::ostream &out, bool isFirst, bool isLast, bool isSpac
     else if (isLast) out << std::endl << "+";
     else out << std::endl << "|";
 
-    for (int i = 0; i < (m_charsPerCell * m_cardSize) - 2; ++i) {
+    for (int i = 0; i < (m_charsPerCell * m_cardSize) - 1; ++i) {
         out << "-";
     }
 
-    if (isFirst) out << "+" << std::endl << "| ";
+    if (isFirst) out << "+" << std::endl << "|";
     else if (isLast) out << "+" << std::endl;
-    else if (isSpacer) out << "|" << std::endl << "| ";
+    else if (isSpacer) out << "|" << std::endl << "|";
 }
 
--- Bingo/Deck.h
+++ Bingo/Deck.h
@@ -38,8 +38,19 @@ private:
 
     /**
      * m_charsPerCell is the amount of monospace characters needed to represent the biggest cell
-     * we need to deal with, in this case, a number consisting of no more than 3
-     * digits plus a space on either side and a '|' on either side needs 7 spaces
+     * we need to deal with, for example, a number consisting of no more than 3
+     * digits can be displayed as follows:
+     *
+     * a space (to look pretty), +1 monospace character
+     * the number, which is at most 3 digits wide, +3 monospace characters
+     * another space for prettiness, +1 monospace character
+     * a '|' to separate the cell from its neighbor, +1 monospace character
+     *
+     * This gives us 6 monospace characters for a cell if m_numberMax < 1000
+     *
+     * Note: when printing we, also have to take into account the need for a '|' at the
+     * beginning of a line, but this is left out of the character count for a cell as it leads to
+     * incorrect arithmetic. It is easier to just hard code it in, which is done in printHelper
      */
     int m_charsPerCell;
 
