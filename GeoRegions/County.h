55691e51a9804c91ea9b8a4796940bc79993ca36
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-03-14 11:37:32 -0600

    Implemented County class

--- GeoRegions/County.h
+++ GeoRegions/County.h
@@ -5,6 +5,13 @@
 #ifndef GEO_REGIONS_COUNTY_H
 #define GEO_REGIONS_COUNTY_H
 
-// TODO: Finish definition for a County class
+#include "Region.h"
+
+#include <string>
+
+class County : public Region {
+public:
+    County(const std::string data[]);
+};
 
 #endif //GEO_REGIONS_COUNTY_H
