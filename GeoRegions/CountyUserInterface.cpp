c621fe8f92bdf28266d6a5a07bec248cd061f9e2
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-03-18 23:29:39 -0600

    Fixed menu message in CountyUserInterface.cpp

--- GeoRegions/CountyUserInterface.cpp
+++ GeoRegions/CountyUserInterface.cpp
@@ -18,7 +18,7 @@ void CountyUserInterface::setupMenu() {
     m_menu->addOption("C", "Create a new city");
     m_menu->addOption("L", "List all cities in this county");
     m_menu->addOption("E", "Edit a city");
-    m_menu->addOption("P", "Print a report containing all counties or cities in this state");
+    m_menu->addOption("P", "Print a report containing all cities in this county");
     m_menu->addOption("D", "Delete a city");
 }
 
