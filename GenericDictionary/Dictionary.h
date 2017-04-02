3b403c93c56c753ef3177d489fa9c8d897c4677b
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-04-01 19:39:51 -0600

    Removed outdated TODO

--- GenericDictionary/Dictionary.h
+++ GenericDictionary/Dictionary.h
@@ -147,7 +147,6 @@ Dictionary<Comparable, ValType>::binaryFindByKey(int const &start, int const &en
     else return binaryFindByKey(start, mid - 1, key);
 }
 
-// TODO: test me
 template<typename Comparable, typename ValType>
 KeyValue<Comparable, ValType> Dictionary<Comparable, ValType>::getByIndex(int const &index) const {
     if (index < 0 || index >= m_validKeys.size()) {
