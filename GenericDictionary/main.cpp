7713c3b7a957984347d6683a5e94de69ab5b5dd0
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-04-04 12:45:33 -0600

    Wrote an example usage of the Dictionary class in main.cpp

--- GenericDictionary/main.cpp
+++ GenericDictionary/main.cpp
@@ -1,28 +1,85 @@
 #include <iostream>
 #include <fstream>
+#include <locale>
+#include <sstream>
 #include "Dictionary.h"
+
+std::stringstream toUpper(std::string const &s);
+
+std::string prompt(bool &cont);
 
 // TODO: write UML
 // TODO: write an example in main
 int main() {
 
-    // Dictionary object for an English dictionary
-    // The key is the word, the value is the definition
-    // except I'm too lazy to actually find a dictionary with definitions, and I'm sure not writing my own.
-    // so just pretend that each one has a real definition.
-    Dictionary<std::string, std::string> dictionary(130000);
+    // A non-comprehensive list of nations in the world
+    Dictionary<std::string, std::string> nations(150);
 
-    std::ifstream fin("./Dictionary.txt");
+    std::ifstream fin("./countrynames.txt");
     if (!fin) {
         std::cerr << "Could not open input file" << std::endl;
+        return EXIT_FAILURE;
     }
 
-    std::string word;
-    std::string definition = "definition goes here"; // Pretend we have real definitions...
+    std::string nation = "";
+    std::string about = "info about country goes here"; // Pretend we have info...
 
-    while (fin >> word) {
-        dictionary.addKeyValue(word, definition);
+    while (!fin.eof()) {
+        std::getline(fin, nation);
+        nations.addKeyValue(toUpper(nation).str(), about);
+    }
+
+    bool cont = true;
+    while (cont) {
+        std::string input = prompt(cont);
+        if (cont) {
+            try {
+                std::string def = nations.getByKey(input).getValue();
+                std::cout << input << ": " << def << std::endl;
+            } catch (std::exception e) {
+                std::cout << "Sorry, could not find that nation." << std::endl << std::endl;
+            }
+        }
     }
 
     return EXIT_SUCCESS;
+}
+
+std::stringstream toUpper(std::string const &s) {
+    std::stringstream ss;
+
+    std::locale loc;
+    for (std::string::size_type i = 0; i < s.length(); ++i) {
+        ss << std::toupper(s[i], loc);
+    }
+    return ss;
+}
+
+std::string prompt(bool &cont) {
+    std::string input = "";
+    std::cout << "What would you like to do?\n"
+              << "\t-  (S)earch for a nation\n"
+              << "\t-  (E)xit" << std::endl;
+
+    std::cin >> input;
+    std::cin.ignore();
+
+    if (input == "S" || input == "s" || input == "Search") {
+        std::string word;
+        std::cout << "Enter a nation to search for (enter to return to menu):";
+        std::getline(std::cin, word);
+
+        if (word != "") {
+            return toUpper(word).str();
+        } else {
+            std::cout << std::endl << std::endl;
+            return toUpper(prompt(cont)).str();
+        }
+    } else if (input == "E" || input == "e" || input == "Exit") {
+        cont = false;
+        return "";
+    } else {
+        std::cout << "Invalid input, try again!" << std::endl;
+        return toUpper(prompt(cont)).str();
+    }
 }
