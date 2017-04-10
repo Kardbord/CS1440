8ae0c72024d4380a412f17ad488d523491e912e3
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-04-10 14:48:43 -0600

    implemented Configuration::getParameter(string const &key)

--- SOLID-Principles/Configuration.cpp
+++ SOLID-Principles/Configuration.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "Configuration.h"
+
+std::string Configuration::getParameter(std::string const &key) const {
+    try {
+        return m_configParameters.at(key);
+    } catch (std::out_of_range e) {
+        // Throw my own exception with my own message for greater clarity as to where things went wrong
+        throw std::out_of_range("Invalid key in Configuration::getParameter(string)");
+    }
+}
