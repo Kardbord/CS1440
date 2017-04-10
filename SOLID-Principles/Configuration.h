920c56d969365ddffdbbe72c0a980a555410cc91
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-04-10 10:51:58 -0600

    Began work on Configuration class

--- SOLID-Principles/Configuration.h
+++ SOLID-Principles/Configuration.h
@@ -5,9 +5,22 @@
 #ifndef SOLID_PRINCIPLES_CONFIGURATION_H
 #define SOLID_PRINCIPLES_CONFIGURATION_H
 
+#include <string>
+#include <map>
 
 class Configuration {
+public:
+    Configuration() : m_configParameters() {}
 
+    // TODO: implement these
+    bool addParameter();
+    bool removeParemeter();
+    std::string getParameter(std::string const &key) const;
+    double getParameter(std::string const &key);
+    // TODO: add function to getParameter as an int (what size int?)
+
+private:
+    std::map<std::string, std::string> m_configParameters;
 };
 
 
