753a22f38769f671a0c7854f25f75eb1b00641e6
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-04-10 14:55:37 -0600

    implemented double Configuration::getParameter

--- SOLID-Principles/Configuration.cpp
+++ SOLID-Principles/Configuration.cpp
@@ -2,6 +2,7 @@
 // Created by Tanner on 4/10/2017.
 //
 
+#include <stdexcept>
 #include "Configuration.h"
 
 std::string Configuration::getParameter(std::string const &key) const {
@@ -9,6 +10,15 @@ std::string Configuration::getParameter(std::string const &key) const {
         return m_configParameters.at(key);
     } catch (std::out_of_range e) {
         // Throw my own exception with my own message for greater clarity as to where things went wrong
-        throw std::out_of_range("Invalid key in Configuration::getParameter(string)");
+        throw std::out_of_range("Invalid key in Configuration::getParameter() : string");
+    }
+}
+
+double Configuration::getParameter(std::string const &key) {
+    try {
+        return std::stod(m_configParameters.at(key));
+    } catch (std::exception e) {
+        // Throw my own exception with my own message for greater clarity as to where things went wrong
+        throw std::out_of_range("Invalid key or no valid conversion in Configuration::getParameter() : double");
     }
 }
