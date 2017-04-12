5235bf79768460d49f46f075cb21906b8cb55fae
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-04-12 10:40:40 -0600

    Laid foundation for ResultSetTester

--- SOLID-Principles/Testing/ResultSetTester.h
+++ SOLID-Principles/Testing/ResultSetTester.h
@@ -5,9 +5,14 @@
 #ifndef SOLID_PRINCIPLES_RESULTSETTESTER_H
 #define SOLID_PRINCIPLES_RESULTSETTESTER_H
 
+#include <ostream>
+#include "../ResultSet.h"
 
 class ResultSetTester {
+public:
+    void testConstructors(std::ostream &out);
 
+    void testPrint(std::ostream &out);
 };
 
 
