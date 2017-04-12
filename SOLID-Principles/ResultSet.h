210dad1b81bf3cfe3ada2527b4f8772b36ae235e
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-04-12 09:55:23 -0600

    Began work on ResultSet class

--- SOLID-Principles/ResultSet.h
+++ SOLID-Principles/ResultSet.h
@@ -5,9 +5,20 @@
 #ifndef SOLID_PRINCIPLES_RESULTSET_H
 #define SOLID_PRINCIPLES_RESULTSET_H
 
+#include <map>
+#include <ostream>
+#include <string>
+#include <vector>
+
 // TODO: this class is a map iwth a string as a key and a container as the value
 class ResultSet {
+public:
+    ResultSet() : m_results() {}
 
+    void print(std::ostream &out) const;
+
+private:
+    std::map<std::string, std::vector<std::string>> m_results;
 };
 
 
