a8f28518cad2c65960f2941f48e0cc1c60f8fcd5
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-04-12 16:45:58 -0600

    Updated Configuration class to inherit from std::map

--- SOLID-Principles/Configuration.cpp
+++ SOLID-Principles/Configuration.cpp
@@ -7,7 +7,7 @@
 
 std::string Configuration::getParamAsString(std::string const &key) const {
     try {
-        return m_configParameters.at(key);
+        return this->at(key);
     } catch (std::out_of_range e) {
         // Throw my own exception with my own message for greater clarity as to where things went wrong
         throw std::out_of_range("Invalid key in Configuration::getParamAsString()");
@@ -15,28 +15,9 @@ std::string Configuration::getParamAsString(std::string const &key) const {
 }
 
 double Configuration::getParamAsDouble(std::string const &key) const {
-    try {
-        return std::stod(m_configParameters.at(key));
-    } catch (std::exception e) {
-        // Throw my own exception with my own message for greater clarity as to where things went wrong
-        throw std::out_of_range("Invalid key or no valid conversion in Configuration::getParamAsDouble()");
-    }
+    return std::stod(this->at(key));
 }
 
 unsigned long long Configuration::getParamAsInt(std::string const &key) const {
-    try {
-        return std::stoull(m_configParameters.at(key));
-    } catch (std::exception e) {
-        // Throw my own exception with my own message for greater clarity as to where things went wrong
-        throw std::out_of_range("Invalid key or no valid conversion in Configuration::getParamAsInt()");
-    }
-}
-
-bool Configuration::addParameter(std::string const &key, std::string const &value) {
-    return m_configParameters.insert(std::pair<std::string, std::string>(key, value)).second;
-}
-
-bool Configuration::removeParemeter(std::string const &key) {
-    unsigned int numRemoved = (unsigned int) m_configParameters.erase(key);
-    return numRemoved == 1;
+        return std::stoull(this->at(key));
 }
--- SOLID-Principles/Configuration.h
+++ SOLID-Principles/Configuration.h
@@ -8,31 +8,15 @@
 #include <string>
 #include <map>
 
-class Configuration {
+// TODO: test me
+class Configuration : public std::map<std::string, std::string> {
 public:
-    Configuration() : m_configParameters() {}
-
-    /**
-     * @param key for the std::pair
-     * @param value for the std::pair
-     * @return true if the new pair was added, false if the key already existed
-     */
-    bool addParameter(std::string const &key, std::string const &value);
-
-    /**
-     * @param key of std::pair to be removed
-     * @return true if the pair was found and deleted
-     */
-    bool removeParemeter(std::string const &key);
 
     std::string getParamAsString(std::string const &key) const;
 
     double getParamAsDouble(std::string const &key) const;
 
     unsigned long long getParamAsInt(std::string const &key) const;
-
-private:
-    std::map<std::string, std::string> m_configParameters;
 };
 
 
