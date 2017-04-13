82187bf7a25147d98cf4c12a6c62624f1e3ef5f8
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-04-12 21:22:47 -0600

    Outlined pure virtuals for Analyzer class

--- SOLID-Principles/Analyzer.h
+++ SOLID-Principles/Analyzer.h
@@ -8,10 +8,18 @@
 
 #include <iosfwd>
 #include "ResultSet.h"
+#include "Configuration.h"
 
 class Analyzer {
 public:
-    virtual ResultSet run(std::istream & in) = 0;
+    virtual ResultSet run(std::istream &in) = 0;
+
+    virtual void configure(Configuration const &config) = 0;
+
+    virtual void configure() = 0;
+
+protected:
+    Configuration m_configuration;
 };
 
 
