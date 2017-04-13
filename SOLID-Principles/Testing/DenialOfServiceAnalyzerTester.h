692a412a5b5424edac629968c6821a3088583537
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-04-12 23:17:22 -0600

    laid foundation for DenialOfServiceAnalyzerTester

--- SOLID-Principles/Testing/DenialOfServiceAnalyzerTester.h
+++ SOLID-Principles/Testing/DenialOfServiceAnalyzerTester.h
@@ -5,9 +5,13 @@
 #ifndef SOLID_PRINCIPLES_DENAILOFSERVICEANALYZERTESTER_H
 #define SOLID_PRINCIPLES_DENAILOFSERVICEANALYZERTESTER_H
 
+#include <ostream>
+#include "../DenialOfServiceAnalyzer.h"
 
 class DenialOfServiceAnalyzerTester {
+    DenialOfServiceAnalyzerTester() {}
 
+    void testConstructorAndGetters(std::ostream &out);
 };
 
 
