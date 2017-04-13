04bcfad3ed16228b0a87f90bbe09157c08c24b32
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-04-13 11:55:18 -0600

    Removed redundant protected method getConfiguration in DenialOfServiceAnalyzer clasS

--- SOLID-Principles/DenialOfServiceAnalyzer.h
+++ SOLID-Principles/DenialOfServiceAnalyzer.h
@@ -21,7 +21,6 @@ public:
     virtual std::vector<std::string> getConfigKeys() const { return m_configKeys; }
 
 protected:
-    virtual Configuration getConfiguration() const { return m_configuration; }
     void configure();
 
     std::vector<std::string> m_configKeys;
