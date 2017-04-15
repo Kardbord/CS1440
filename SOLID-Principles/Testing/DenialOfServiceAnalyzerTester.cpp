5853a520ac88d7c028de6566d412f01b824a6040
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-04-14 19:16:15 -0600

    rewrote tester for DenialOfServiceAnalyzer

--- SOLID-Principles/Testing/DenialOfServiceAnalyzerTester.cpp
+++ SOLID-Principles/Testing/DenialOfServiceAnalyzerTester.cpp
@@ -9,38 +9,33 @@ void DenialOfServiceAnalyzerTester::testConstructorAndGetters(std::ostream &out)
 
     DenialOfServiceAnalyzer analyzer("20", "30", "40");
 
+    Configuration analyzerConfig = analyzer.getConfiguration();
+
     try {
-        if (analyzer.getConfigPair("Timeframe").second != "20") {
-            out << "Failure in constructor, Timeframe config parameter should be \"20\"" << std::endl;
+        if (analyzerConfig.getParamAsString("Timeframe") != "20") {
+            out << "Failure in constructor, values are not as assigned" << std::endl;
             return;
         }
 
-        if (analyzer.getConfigPair("Likely Attack Message Count").second != "30") {
-            out << "Failure in constructor, Likely Attack Message Count config parameter should be \"30\"" << std::endl;
+        if (analyzerConfig.getParamAsString("Likely Attack Message Count") != "30") {
+            out << "Failure in constructor, values are not as assigned" << std::endl;
             return;
         }
 
-        if (analyzer.getConfigPair("Possible Attack Message Count").second != "40") {
-            out << "Failure in constructor, Possible Attack Message Count config parameter should be \"40\""
-                << std::endl;
+        if (analyzerConfig.getParamAsString("Possible Attack Message Count") != "40") {
+            out << "Failure in constructor, values are not as assigned" << std::endl;
             return;
         }
 
         try {
-            analyzer.getConfigPair("adsf");
-            out << "Failure in getConfigPair(\"asdf\"), should have thrown exception" << std::endl;
-        } catch (std::out_of_range e) {
-            // do nothing, this is expected behavior
+            analyzerConfig.getParamAsString("not a real parameter");
+            out << "Failure in constructor, configParam exists that should not" << std::endl;
+            return;
+        } catch (std::exception e) {
+            // Do nothing, this is expected behavior
         }
 
-        try {
-            analyzer.getConfigPair("1");
-            out << "Failure in getConfigPair(\"1\"), should have thrown exception" << std::endl;
-        } catch (std::out_of_range e) {
-            // do nothing, this is expected behavior
-        }
-
-    } catch (std::out_of_range e) {
-        out << "Failure while testing getConfigPair, threw an unexpected exception" << std::endl;
+    } catch (std::exception e) {
+        out << "Failure in testConstructorAndGetters, threw unexpected exception" << std::endl;
     }
 }
