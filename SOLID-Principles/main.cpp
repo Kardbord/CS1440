d23493b98c039ab4121660b0a9e87c8cf1f66a46
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-04-15 21:29:29 -0600

    Added a scope to main.cpp for future use

--- SOLID-Principles/main.cpp
+++ SOLID-Principles/main.cpp
@@ -3,13 +3,16 @@
 #include <fstream>
 
 int main() {
-    PortScanAnalyzer portScanAnalyzer("50", "2");
 
-    std::ifstream fin("./SampleData.csv");
-    if (!fin) {
-        std::cout << "Could not open input file. Shutting down..." << std::endl;
+    {
+        PortScanAnalyzer portScanAnalyzer("50", "2");
+
+        std::ifstream fin("./SampleData.csv");
+        if (!fin) {
+            std::cout << "Could not open input file. Shutting down..." << std::endl;
+        }
+
+        portScanAnalyzer.run(fin).print(std::cout);
+        fin.close();
     }
-
-    portScanAnalyzer.run(fin).print(std::cout);
-    fin.close();
 }
